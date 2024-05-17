e2cc1f3a5c66e1e482dd13f7d429704e95100d9f
Your Name <you@example.com>
2024-05-17 19:01:54 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical ChatBOT',
+    version='0.0.0',
+    author='Upanshu Sharma',
+    author_email='upanshusharma0@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
